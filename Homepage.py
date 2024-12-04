ee96aa9b2a4232bec0da3cbf3dafe649f7c6723a
Yanelly Hernandez <100453560+yhernandez55@users.noreply.github.com>
2024-12-04 14:26:47 -0800

    Inputong the py files of streamlit.'

---
+++ Homepage.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Page Configuration
+st.set_page_config(
+    page_title="Yanelly's Data Science Portfolio",
+    page_icon=":)",
+)
+
+# Title of the Homepage
+st.title("Welcome to Yanelly's Portfolio")
+
